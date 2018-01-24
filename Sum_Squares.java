3e885a3b8aca5712a9913d128b0c18808bf3b93e
Perry <srperry.2017@mymail.jcsu.edu>
2018-01-24 15:22:42 -0500

    first commit

---
+++ Sum_Squares.java
@@ -0,0 +1,19 @@
+
+public class Sum_Squares {
+
+	public static void main(String[] args) {
+		int i;
+		int sum=0;
+	for(i=1;i<=10;i++){
+		System.out.println(i);
+		int square=(i*i);
+		sum= (sum+square);
+	}
+		
+		
+		
+		
+System.out.println(sum);
+	}
+
+}
